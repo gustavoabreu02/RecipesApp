8c2c4bafc9ad8ea1c7a3a3722fde378eff1b75e3
Lanna Antolin <97650982+L-Antolin@users.noreply.github.com>
2022-07-18 15:36:17 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
         <Route path="/profile" component={ Profile } />
         <Route path="/done-recipes" component={ DoneRecipes } />
         <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+        <Route exact path="/" component={ Login } />
       </Switch>
     </div>
   );
