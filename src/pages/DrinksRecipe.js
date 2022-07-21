dd01fa17b6de8a127b8d349432355a9f9ede8334
Lanna Antolin <lanna.antolin@gmail.com>
2022-07-21 18:43:33 -0300

    Requisito 28 refatorado, 32 feito, 35 somente importação

--- src/pages/DrinksRecipe.js
+++ src/pages/DrinksRecipe.js
@@ -4,16 +4,7 @@ import RecipeDetails from '../components/RecipeDetails';
 class DrinksRecipe extends React.Component {
   render() {
     return (
-      <>
-        <RecipeDetails />
-        <button
-          className="startRecipeBtn"
-          data-testid="start-recipe-btn"
-          type="button"
-        >
-          Start Recipe
-        </button>
-      </>
+      <RecipeDetails />
     );
   }
 }
--- src/pages/FoodsRecipe.js
+++ src/pages/FoodsRecipe.js
@@ -4,16 +4,7 @@ import RecipeDetails from '../components/RecipeDetails';
 class FoodsRecipe extends React.Component {
   render() {
     return (
-      <>
-        <RecipeDetails />
-        <button
-          className="startRecipeBtn"
-          data-testid="start-recipe-btn"
-          type="button"
-        >
-          Start Recipe
-        </button>
-      </>
+      <RecipeDetails />
     );
   }
 }
